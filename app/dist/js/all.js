2fe8f45c0d4b543b786732f1f973bfdb99129003
ysai.pro@gmail.com <ysai.pro@gmail.com>
2021-09-03 09:36:42 +0900

    Add edit card

--- app/dist/js/all.js
+++ app/dist/js/all.js
@@ -1,1 +1,36 @@
-import 'bootstrap';
+document.getElementById("card-expand").addEventListener("click",function (){
+    
+    let expandArrow = document.getElementById("card-expand");
+    let buttons = document.querySelector("#card-buttons");
+    if(!expandArrow.classList.contains("expand")){
+        if(expandArrow.classList.contains("restract")){expandArrow.classList.remove("restract");}
+        expandArrow.classList.add("expand");
+    }else{
+        expandArrow.classList.remove("expand");
+        expandArrow.classList.add("restract");
+    }
+    if(!buttons.classList.contains("show-buttons")){
+        if(buttons.classList.contains("hide-buttons")){
+            buttons.classList.remove("hide-buttons");
+        }
+        buttons.classList.add("show-buttons");
+    }else{
+        buttons.classList.remove("show-buttons");
+        buttons.classList.add("hide-buttons");
+    }
+    
+})
+
+function toggleFullScreen(){
+    document.getElementById("full-screen").classList.toggle("d-none");
+}
+document.getElementById("iconClear").addEventListener("click",function(){
+    toggleFullScreen();
+});
+
+let buttons_edit = document.querySelectorAll(".icon__watch");
+for(let i = 0;i < buttons_edit.length ; i ++){
+    buttons_edit[i].addEventListener("click",function (){
+        toggleFullScreen();
+    });
+}
